ae89e977f52711e65b4c2e9740ec1e4a8fddfc5e
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-16 11:11:59 +0330

    fix (component) : input lable

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,8 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
-    {{ $value ?? $slot }}
-</label>
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-white inline-block"></span>
+    <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+        {{ $value ?? $slot }}
+    </label>
+</div>
