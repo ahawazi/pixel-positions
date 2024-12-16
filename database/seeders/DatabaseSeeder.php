7500d0804c195ac1143223e63d751a9034c4efe9
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-16 07:02:37 +0330

    feat : seeder & factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             EmployerSeeder::class,
             JobSeeder::class,
+            // TagSeeder::class,
         ]);
     }
 }
