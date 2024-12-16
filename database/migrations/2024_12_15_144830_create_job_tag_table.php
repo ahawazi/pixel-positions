5d1316905a01c3c30baf4125077e818fbcf77b2a
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-16 07:00:20 +0330

    feat (migration) : job_tag

---
+++ database/migrations/2024_12_15_144830_create_job_tag_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Job;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignIdFor(Job::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('job_tag');
+    }
+};
