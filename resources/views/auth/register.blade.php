f32651ed123f246fccea9cb416e14e4621cbf3c5
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-16 11:11:00 +0330

    fix : add fild employer in register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,8 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+<x-site-layout>
+    <form method="POST" action="{{ route('register') }}" class="max-w-2xl mx-auto space-y-6" enctype="multipart/form-data">
         @csrf
+
+        <x-page-heading>Register</x-page-heading>
 
         <!-- Name -->
         <div>
@@ -39,14 +41,31 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+        <x-divider />
+
+        {{-- Employer Name--}}
+        <div class="mt-4">
+            <x-input-label for="employer_name" :value="__('Employer Name')" />
+            <x-text-input id="employer_name" type="text" name="employer_name" :value="old('employer_name')" required autocomplete="employer_name" />
+            <x-input-error :messages="$errors->get('employer_name')" class="mt-2" />
+        </div>
+
+        {{-- Employer Logo--}}
+        <div class="mt-4">
+            <x-input-label for="employer_logo" :value="__('Employer Logo')" />
+            <x-text-input id="employer_logo" type="file" name="logo" :value="old('employer_logo')" required autocomplete="employer_logo" accept=".png,.jpg,.webp"/>
+            <x-input-error :messages="$errors->get('employer_logo')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-start mt-4">
+            <x-primary-button>
+                {{ __('Register') }}
+            </x-primary-button>
+
+            <a class="underline ms-4 text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-site-layout>
