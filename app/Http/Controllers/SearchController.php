15aa5875a18b236103ab6f6e7955fdad358b4fa7
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-18 21:53:11 +0330

    fix query

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,8 +9,11 @@ class SearchController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $jobs = Job::where('title', 'LIKE', '%'.request('q').'%')->get();
-
+        $jobs = Job::query()
+        ->with(['employer', 'tags'])
+        ->where('title', 'LIKE', '%'.request('q').'%')
+        ->get();
+        
         return view('components.results', [
             'jobs' => $jobs,
         ]);
