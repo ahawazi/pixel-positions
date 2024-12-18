e83b9bf9c7222b0a79cf508176d10469bac48016
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-18 14:30:59 +0330

    feat (tag) : complited

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/', JobController::class);
 
 Route::get('/search', SearchController::class)->name('search');
+
+Route::get('/tags/{tag:name}', TagController::class)->name('tag');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
